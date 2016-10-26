619cd0969c258279504584222f0c22e438337751
Andriy <andriy.rom1294@gmail.com>
2016-10-26 12:12:06 +0300

    Added realization and tests of round keys generator for DES

---
+++ SymetricCiphers.Test/DesKeyGeneratorTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SymetricCiphers.DES;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetricCiphers.Test {
+    [TestClass]
+    public class DesKeyGeneratorTest {
+        [TestMethod]
+        public void TestCopyBitArray() {
+            BitArray input = new BitArray(new byte[] { 5 });
+            BitArray result = new BitArray(input.Length);
+            KeyGenerator.CopyBitArray(input, 0, result, 0, 4);
+            KeyGenerator.CopyBitArray(input, 0, result, 4, 4);
+            BitArray expected = new BitArray(new byte[] { 85 });
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestCycleLeftShiftOneStep() {
+            BitArray input = new BitArray(new byte[] { 11 });
+            BitArray result = KeyGenerator.CycleLeftShift(input, 1);            
+            BitArray expected = new BitArray(new byte[] { 133 });
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestCycleLeftShiftTwoStep() {
+            BitArray input = new BitArray(new byte[] { 11 });
+            BitArray result = KeyGenerator.CycleLeftShift(input, 2);
+            BitArray expected = new BitArray(new byte[] { 194 });
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestKeyGenerator() {
+            string[] expectedSubKeys = new string[] {
+                "000110 110000 001011 101111 111111 000111 000001 110010",
+                "011110 011010 111011 011001 110110 111100 100111 100101",
+                "010101 011111 110010 001010 010000 101100 111110 011001",
+                "011100 101010 110111 010110 110110 110011 010100 011101",
+                "011111 001110 110000 000111 111010 110101 001110 101000",
+                "011000 111010 010100 111110 010100 000111 101100 101111",
+                "111011 001000 010010 110111 111101 100001 100010 111100",
+                "111101 111000 101000 111010 110000 010011 101111 111011",
+                "111000 001101 101111 101011 111011 011110 011110 000001",
+                "101100 011111 001101 000111 101110 100100 011001 001111",
+                "001000 010101 111111 010011 110111 101101 001110 000110",
+                "011101 010111 000111 110101 100101 000110 011111 101001",
+                "100101 111100 010111 010001 111110 101011 101001 000001",
+                "010111 110100 001110 110111 111100 101110 011100 111010",
+                "101111 111001 000110 001101 001111 010011 111100 001010",
+                "110010 110011 110110 001011 000011 100001 011111 110101"
+            };
+            string stringKey = "00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001";
+            BitArray key = StringToBitArray(stringKey.Replace(" ", ""));
+            KeyGenerator keyGenerator = new KeyGenerator(key);
+            for (int i = 0; i < KeyGenerator.NumberOfRounds; i++) {
+                BitArray result = keyGenerator.GetRoundKey(i);
+                BitArray expected = StringToBitArray(expectedSubKeys[i].Replace(" ", ""));
+                CollectionAssert.AreEqual(expected, result, string.Format("Expected and result key number {0} do not match", i));
+            }
+        }
+
+        private string BitArrayToString(BitArray array) {
+            StringBuilder bitString = new StringBuilder(array.Length);
+            foreach (bool bit in array) {
+                bitString.Append(bit ? "1" : "0");
+            }
+            return bitString.ToString();
+        }
+
+        private BitArray StringToBitArray(string bitString) {
+            BitArray array = new BitArray(bitString.Length);
+            for (int i = 0; i < bitString.Length; i++) {
+                switch (bitString[i]) {
+                    case '0': array[i] = false;
+                        break;
+                    case '1': array[i] = true;
+                        break;
+                    default: throw new ArgumentException("Input ");
+                }
+            }
+            return array;    
+        }
+    }
+}
--- SymetricCiphers/DES/KeyGenerator.cs
+++ SymetricCiphers/DES/KeyGenerator.cs
@@ -1,20 +1,85 @@
-﻿using System;
+﻿using SymetricCiphers.Common;
+using System;
 using System.Collections;
 
 namespace SymetricCiphers.DES {
     internal class KeyGenerator {
-        private static readonly int NumberOfRounds = 16;
+        public static readonly int NumberOfRounds = 16;
         private static readonly int[] InitialKeyPermutationVector = 
-            new int[] { 49, 42, 35, 28, 21, 14, 7, 0, 50, 43, 36, 29, 22, 15, 
-                        8, 1, 51, 44, 37, 30, 23, 16, 9, 2, 52, 45, 38, 31,
-                        55, 48, 41, 34, 27, 20, 13, 6, 54, 47, 40, 33, 26, 19,
-                        12, 5, 53, 46, 39, 32, 26, 18, 11, 4, 24, 17, 10, 3 };
+            new int[] { 56, 48, 40, 32, 24, 16, 8, 0, 57, 49, 41, 33, 25, 17, 
+                        9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35,
+                        62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21,
+                        13, 5, 60, 52, 44, 36, 28, 20, 12, 4, 27, 19, 11, 3 };
+        private static readonly int[] CompressionPermutationVector =
+            new int[] { 13, 16, 10, 23, 0, 4, 2, 27, 14, 5, 20, 9, 22, 18, 11, 3,
+                        25, 7, 15, 6, 26, 19, 12, 1, 40, 51, 30, 36, 46, 54, 29, 39,
+                        50, 44, 32, 47, 43, 48, 38, 55, 33, 52, 45, 41, 49, 35, 28, 31 };
+        
+        private BitArray[] RoundKeys = new BitArray[NumberOfRounds];
 
-        BitArray[] RoundKeys = new BitArray[NumberOfRounds];
+        private BitArray PartC = new BitArray(28);
+        private BitArray PartD = new BitArray(28);
         public KeyGenerator(BitArray key) {
-            if (key.Length != 56) {
-                throw new ArgumentException("Key should have length 56 bits."); 
+            if (key.Length != 64) {
+                throw new ArgumentException("Key should have length 64 bits."); 
             }
+            CreateRoundKeys(key);
+        }
+
+        public BitArray GetRoundKey(int roundNumber) {
+            return RoundKeys[roundNumber];
+        }
+
+        private void CreateRoundKeys(BitArray key) {
+            Permutator initialPermutation = new Permutator(InitialKeyPermutationVector);
+            BitArray permutatedKey = initialPermutation.Permut(key);
+            Permutator finalPermutator = new Permutator(CompressionPermutationVector);
+            FillPartsCD(permutatedKey);
+            for (int i = 0; i < NumberOfRounds; i++) {
+                int shiftSize = GetShiftOnStep(i);
+                PartC = CycleLeftShift(PartC, shiftSize);
+                PartD = CycleLeftShift(PartD, shiftSize);
+                BitArray concatenatedCDParts = ConcatCDPArts();
+                RoundKeys[i] = finalPermutator.Permut(concatenatedCDParts);
+            }
+        }
+
+        private int GetShiftOnStep(int stepNumber) {
+            switch (stepNumber) {
+                case 0: 
+                case 1:
+                case 8:
+                case 15:
+                    return 1;
+                default:
+                    return 2;
+            }
+        }
+
+        private void FillPartsCD(BitArray permutatedKey) {
+            CopyBitArray(permutatedKey, 0, PartC, 0, 28);
+            CopyBitArray(permutatedKey, 28, PartD, 0, 28);
+        }
+
+        private BitArray ConcatCDPArts() {
+            int resultLength = PartC.Length + PartD.Length;
+            var result = new BitArray(resultLength);
+            CopyBitArray(PartC, 0, result, 0, PartC.Length);
+            CopyBitArray(PartD, 0, result, PartC.Length, PartD.Length);
+            return result;
+        }
+
+        internal static void CopyBitArray(BitArray source, int sourceIndex, BitArray destination, int destinationIndex, int length) {
+            for (int i = 0; i < length; i++) {
+                destination[destinationIndex + i] = source[sourceIndex + i];
+            }
+        }
+
+        internal static BitArray CycleLeftShift(BitArray array, int shiftSize) {
+            var result = new BitArray(array.Length);
+            CopyBitArray(array, shiftSize, result, 0, array.Length - shiftSize);
+            CopyBitArray(array, 0, result, array.Length - shiftSize, shiftSize);
+            return result;
         }
     }
 }
