2282c6fa182f85c4beba236e23e22398d618b458
Andriy <andriy.rom1294@gmail.com>
2016-11-07 00:08:56 +0200

    Implemented logic for aligning cipher text and splitting it into blocks

--- SymetricCiphers/DES/DesCipher.cs
+++ SymetricCiphers/DES/DesCipher.cs
@@ -12,13 +12,36 @@ namespace SymetricCiphers.DES {
         }
 
         public byte[] Key { get; private set; }
-
         public byte[] Encrypt(byte[] message) {
+            message = AlignMessage(message);
+            byte[] result = new byte[message.Length];
+            int blockCount = message.Length / BlockLength;
+            for (int i = 0; i < blockCount; i++) {
+                byte[] block = new byte[BlockLength];
+                int blockStartIndex = i * BlockLength;
+                Array.Copy(message, blockStartIndex, block, 0, BlockLength);
+                byte[] encryptedBlock = EncryptBlock(block);
+                encryptedBlock.CopyTo(result, blockStartIndex);
+            }
+            return result;
+        }
+
+        private byte[] EncryptBlock(byte[] block) {
             throw new NotImplementedException();
         }
-                
+
         public byte[] Decrypt(byte[] encryptedMessage) {
             throw new NotImplementedException();
+        }
+
+        private byte[] AlignMessage(byte[] message) {
+            int tail = message.Length % BlockLength;
+            if (tail == 0) { return message; }
+            int alignedMessageLength = (message.Length / BlockLength + 1) * BlockLength;
+            byte[] alignedMessage = new byte[alignedMessageLength];
+            alignedMessage.Initialize();
+            message.CopyTo(alignedMessage, 0);
+            return alignedMessage;
         }
     }
 }
