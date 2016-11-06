2573bce8614b77a60a9e865f55536e8eacd734a4
Andriy <andriy.rom1294@gmail.com>
2016-11-07 00:00:30 +0200

    Added common cipher interface and DesCipher without implementation

---
+++ SymetricCiphers/DES/DesCipher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetricCiphers.DES {
+    public class DesCipher : ICipher {
+        private const int BlockLength = 8;
+        public DesCipher(byte[] key) {
+            Key = key;
+        }
+
+        public byte[] Key { get; private set; }
+
+        public byte[] Encrypt(byte[] message) {
+            throw new NotImplementedException();
+        }
+                
+        public byte[] Decrypt(byte[] encryptedMessage) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SymetricCiphers/ICipher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetricCiphers {
+    interface ICipher {
+        byte[] Key { get; }
+
+        byte[] Encrypt(byte[] message);
+
+        byte[] Decrypt(byte[] encryptedMessage);
+    }
+}
