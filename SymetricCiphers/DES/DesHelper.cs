225c7386866f3e55be996cbfe155eb90e78be001
Andriy <andriy.rom1294@gmail.com>
2016-11-13 14:57:19 +0200

    DesHelper permutation vectors were made private and added permutators for each vector.

--- SymetricCiphers/DES/DesHelper.cs
+++ SymetricCiphers/DES/DesHelper.cs
@@ -1,28 +1,38 @@
-﻿using System;
+﻿using SymetricCiphers.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SymetricCiphers.DES {
-    internal class DesHelper {
-        public static readonly byte[] InitialPermutation = 
-            new byte[] { 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 25, 27, 19, 11, 3,
-                         61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7,
-                         56, 48, 40, 32, 24, 16, 8, 0, 58, 50, 42, 34, 26, 18, 10, 2,
-                         60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6 };
+    internal static class DesHelper {
+        public static readonly Permutator InitialPermutator = new Permutator(InitialPermutation);
+        public static readonly Permutator FinalPermutator = new Permutator(FinalPermutation);
+        public static readonly Permutator PBlockExtensionPermutator = new Permutator(PBlockExtensionPermutation);
+        public static readonly Permutator PBlockStraightPermutator = new Permutator(PBlockStraightPermutation);
 
-        public static readonly byte[] FinalPermutation = 
-            new byte[] { 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30,
-                         37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 
-                         35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26,
-                         33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48, 16, 56, 24 };
+        private static readonly int[] InitialPermutation = 
+            new int[] { 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 25, 27, 19, 11, 3,
+                        61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7,
+                        56, 48, 40, 32, 24, 16, 8, 0, 58, 50, 42, 34, 26, 18, 10, 2,
+                        60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6 };
 
-        public static readonly byte[] PBlockExtensionPermutation =
-            new byte[] { 31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8,
-                         7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16,
-                         15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24,
-                         23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0 };
+        private static readonly int[] FinalPermutation = 
+            new int[] { 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30,
+                        37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 
+                        35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26,
+                        33, 1, 41, 9, 49, 17, 57, 25, 32, 0, 40, 8, 48, 16, 56, 24 };
+
+        private static readonly int[] PBlockExtensionPermutation =
+            new int[] { 31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8,
+                        7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16,
+                        15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24,
+                        23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0 };
+
+        private static readonly int[] PBlockStraightPermutation =
+           new int[] { 15, 6, 19, 20, 28, 11, 27, 16, 0, 14, 22, 25, 4, 17, 30, 9,
+                       1, 7, 23, 13, 31, 26, 2, 8, 18, 12, 29, 5, 21, 10, 3, 24 };
 
         internal class SBoxes {
             private static readonly byte[][][] SBoxes = new byte[][][] { 
