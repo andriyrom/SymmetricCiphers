e9b8fd3a5a2f7cfc01ae0697da37f02c3353ec19
Andriy <andriy.rom1294@gmail.com>
2016-12-05 12:31:24 +0200

    Added test for DesCipher decryption

--- SymetricCiphers.Test/DesCipherTest.cs
+++ SymetricCiphers.Test/DesCipherTest.cs
@@ -16,5 +16,17 @@ namespace SymetricCiphers.Test {
 
             CollectionAssert.AreEqual(expectedEncryptedMessage, actualEncryptedMessage);
         }
+
+        [TestMethod]
+        public void TestDesCipherDecrypt() {
+            byte[] key = new byte[] { 0x13, 0x34, 0x57, 0x79, 0x9B, 0xBC, 0xDF, 0xF1 };
+            byte[] expectedDecryptedmessage = new byte[] { 0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF };
+            byte[] encryptedMessage = new byte[] { 0x85, 0xE8, 0x13, 0x54, 0x0F, 0x0A, 0xB4, 0x05 };
+
+            ICipher cipher = new DesCipher(key);
+            byte[] actualDecryptedMessage = cipher.Decrypt(encryptedMessage);
+
+            CollectionAssert.AreEqual(expectedDecryptedmessage, actualDecryptedMessage);
+        }
     }
 }
